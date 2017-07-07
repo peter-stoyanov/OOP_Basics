fd144840c690a76863e31101f219b566edbb1735
Petar Stoyanov <pesho.stoyanov@gmail.com>
2017-07-07 21:44:15 +0300

    inheritance lab

---
+++ 03 Inheritance Lab/RandomList/RandomList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class RandomList : ArrayList
+{
+    private Random rnd;
+
+    public RandomList()
+    {
+        this.rnd = new Random();
+    }
+    
+    /// <summary>
+    /// Removes random element from the list
+    /// </summary>
+    /// <returns></returns>
+    public string RandomString()
+    {
+        int indexToRemove = rnd.Next(0, base.Count - 1);
+
+        var returned = this[indexToRemove];
+
+        base.RemoveAt(indexToRemove);
+
+        return returned.ToString();
+
+    }
+}
---
+++ 03 Inheritance Lab/StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class StackOfStrings
+{
+    private List<string> collection;
+
+    public StackOfStrings()
+    {
+        this.collection = new List<string>();
+    }
+
+    public void Push(string item)
+    {
+        this.collection.Add(item);
+    }
+
+    public string Pop()
+    {
+        var result = this.collection.Last();
+        this.collection.RemoveAt(this.collection.Count - 1);
+        return result;
+    }
+
+    public string Peek()
+    {
+        return this.collection.Last();
+    }
+
+    public bool IsEmpty()
+    {
+        return this.collection.Count == 0;
+    }
+}
